9f97d30e0dacf5a6e04d1ce7013b179c258da7ee
sn0wf1llin <sn0wf1llin@gmail.com>
2020-09-21 01:45:19 +0300

    ;

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@
 PYTHON3_BIN=`which python3`
 
 if [ -z $PYTHON3_BIN ]; then echo -e "[\e[91m!\e[0m] python3 required\n[\e[91m!\e[0m] Install python3 and restart the installation"; exit 1; fi
-$PYTHON3_BIN toro2/toro2.py installnobackup
+$PYTHON3_BIN /opt/toro2/toro2/toro2.py installnobackup
